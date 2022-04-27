83307f4ec2e5d0aadd1bdd01d0c75cc79b441975
Ziyuan Zhao <zhaoziyuan2000@gmail.com>
2022-04-26 20:07:58 -0400

    Minor fix

--- simclr_utitlities.py
+++ simclr_utitlities.py
@@ -287,6 +287,7 @@ def evaluate_model_simple(pred, truth, is_one_hot=True, return_dict=True):
 
     test_cm = sklearn.metrics.confusion_matrix(truth_argmax, pred_argmax)
     test_auroc = sklearn.metrics.roc_auc_score(truth, pred, multi_class='ovr')
+    test_auprc = sklearn.metrics.average_precision_score(truth, pred)
     test_acc = sklearn.metrics.accuracy_score(truth_argmax, pred_argmax)
     test_f1 = sklearn.metrics.f1_score(truth_argmax, pred_argmax, average='macro')
     test_precision = sklearn.metrics.precision_score(truth_argmax, pred_argmax, average='macro')
@@ -300,6 +301,7 @@ def evaluate_model_simple(pred, truth, is_one_hot=True, return_dict=True):
         return {
             'Accuracy': test_acc,
             'AUROC': test_auroc,
+            'AUPRC': test_auprc,
             'Confusion Matrix': test_cm, 
             'F1 Macro': test_f1, 
             'F1 Micro': test_f1_micro, 
