0ebb4d4d00c8c411a35273703f38e11e5e1d0e3e
Ziyuan Zhao <zhaoziyuan2000@gmail.com>
2022-05-08 19:59:15 -0400

    minor fixes

--- AHAR/crop.py
+++ AHAR/crop.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-tr = np.load('full/train.npy')
-va = np.load('full/val.npy')
-te = np.load('full/test.npy')
+tr = np.load('full/train_x.npy')
+va = np.load('full/val_x.npy')
+te = np.load('full/test_x.npy')
 
 window_len = 178
 tr = tr[:,:window_len,0:1]
 va = va[:,:window_len,0:1]
 te = te[:,:window_len,0:1]
 
-np.save('train.npy', tr)
-np.save('val.npy', va)
-np.save('test.npy', te)
+np.save('train_x.npy', tr)
+np.save('val_x.npy', va)
+np.save('test_x.npy', te)
